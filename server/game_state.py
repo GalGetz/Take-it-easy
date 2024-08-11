e0300c4efc0846277db0c2f8e991519c7024cf4d
amitbe711 <amit.benbenishti@mail.huji.ac.il>
2024-08-11 17:14:24 +0300

    Change the seq2idx

--- server/game_state.py
+++ server/game_state.py
@@ -1,47 +1,67 @@
 import copy
 import numpy as np
 
-DEFAULT_BOARD_SIZE = (9, 5)
+# Global seq_to_idx dictionary
+seq_to_idx = {
+    "0_l": [0, 1, 3],
+    "2_l": [2, 4, 6, 8],
+    "5_l": [5, 7, 9, 11, 13],
+    "10_l": [10, 12, 14, 16],
+    "15_l": [15, 17, 18],
+    "3_d": [3, 8, 13],
+    "1_d": [1, 6, 11, 16],
+    "0_d": [0, 4, 9, 14, 18],
+    "2_d": [2, 7, 12, 17],
+    "5_d": [5, 10, 15],
+    "0_r": [0, 2, 5],
+    "1_r": [1, 4, 7, 10],
+    "3_r": [3, 6, 9, 12, 15],
+    "8_r": [8, 11, 14, 17],
+    "13_r": [13, 16, 18]
+}
 
-class GameState(object):
-    def __init__(self, rows=9, columns=5, board_layout=None, board=None, score=0, done=False, remaining_tiles=None):
+# Global idx_to_seq list
+idx_to_seq = [
+    ["0_l", "0_d", "0_r"],  # Index 0
+    ["0_l", "1_d", "1_r"],  # Index 1
+    ["0_r", "2_d", "2_l"],  # Index 2
+    ["3_d", "0_l", "3_r"],  # Index 3
+    ["2_l", "0_d", "1_r"],  # Index 4
+    ["5_l", "0_r", "5_d"],  # Index 5
+    ["2_l", "1_d", "3_r"],  # Index 6
+    ["2_d", "1_r", "5_l"],  # Index 7
+    ["2_l", "3_d", "8_r"],  # Index 8
+    ["0_d", "5_l", "3_r"],  # Index 9
+    ["10_l", "5_d", "1_r"], # Index 10
+    ["5_l", "1_d", "8_r"],  # Index 11
+    ["2_d", "10_l", "3_r"], # Index 12
+    ["5_l", "3_d", "13_r"], # Index 13
+    ["0_d", "10_l", "8_r"], # Index 14
+    ["15_l", "5_d", "3_r"], # Index 15
+    ["1_d", "10_l", "13_r"],# Index 16
+    ["2_d", "15_l", "8_r"], # Index 17
+    ["0_d", "15_l", "13_r"] # Index 18
+]
+
+DEFAULT_BOARD_SIZE = 19  # We now represent the board as a list of 19 elements
+
+class GameState:
+    def __init__(self, board=None, score=0, done=False, remaining_tiles=None):
         super(GameState, self).__init__()
         self._done = done
         self._score = score
 
-        # Default board layout if none is provided
-        if board_layout is None:
-            board_layout = [
-                [0, 0, 1, 0, 0],
-                [0, 1, 0, 1, 0],
-                [1, 0, 1, 0, 1],
-                [0, 1, 0, 1, 0],
-                [1, 0, 1, 0, 1],
-                [0, 1, 0, 1, 0],
-                [1, 0, 1, 0, 1],
-                [0, 1, 0, 1, 0],
-                [0, 0, 1, 0, 0]
-            ]
-
-        self._board_layout = np.array(board_layout)
-
+        # Initialize the board as a list of 19 None elements if not provided
         if board is None:
-            # Initialize the board with None, except for positions where board_layout is 1
-            board = np.full((rows, columns), None, dtype=object)
-            for r in range(rows):
-                for c in range(columns):
-                    if board_layout[r][c] == 1:
-                        board[r][c] = [None, None, None]  # Placeholder for [vertical, left, right]
+            board = [None] * DEFAULT_BOARD_SIZE
 
         self._board = board
-        self._num_of_rows, self._num_of_columns = rows, columns
 
-        # Initialize tiles if not provided
+        # Initialize remaining tiles as a SortedSet if not provided
         if remaining_tiles is None:
-            self._tiles = self.generate_tiles()
-            np.random.shuffle(self._tiles)
+            self._tiles = SortedSet(self.generate_tiles())
         else:
-            self._tiles = remaining_tiles
+            self._tiles = SortedSet(remaining_tiles)
 
     def generate_tiles(self):
         i_values = [1, 5, 9]
@@ -51,8 +71,14 @@ class GameState(object):
         for i in i_values:
             for j in j_values:
                 for k in k_values:
-                    tiles.append([i, j, k])
+                    tiles.append((i, j, k))  # Use tuples to store tiles
         return tiles
+
+    def pop_random_tile(self, index):
+        """Pop and return the tile at the given index from the sorted set of remaining tiles."""
+        if not self._tiles or index < 0 or index >= len(self._tiles):
+            return None
+        return self._tiles.pop(index)  # Remove and return the tile at the given index
 
     @property
     def done(self):
@@ -71,84 +97,47 @@ class GameState(object):
         return self._tiles
 
     def get_legal_actions(self):
-        empty_tiles = self.get_empty_tiles()
-        legal_actions = []
-        for i in range(empty_tiles[0].size):
-            row, col = empty_tiles[0][i], empty_tiles[1][i]
-            legal_actions.append((row, col))
-        return legal_actions
+        # Legal actions are the indices on the board that are still None
+        return [i for i, tile in enumerate(self._board) if tile is None]
 
-    def get_empty_tiles(self):
-        return np.where(self._board == [None, None, None])
-
-    def draw_tile(self):
-        if self._tiles:
-            return self._tiles.pop()
-        return None
-
-    def apply_action(self, chosen_loc, tile):
-        row, col = chosen_loc
-        if self._board[row][col] != [None, None, None]:
-            raise Exception("Illegal action: Tile placement on an already occupied or illegal spot.")
-        self._board[row][col] = tile
-        if self.get_empty_tiles()[0].size == 0:
+    def apply_action(self, idx, tile):
+        if self._board[idx] is not None:
+            raise Exception("Illegal action: Tile placement on an already occupied spot.")
+        self._board[idx] = tile
+        if all(t is not None for t in self._board):  # If no empty spots left, game is done
             self._done = True
+        #todo: update the score each time we put a tile
 
-    def generate_successor(self, chosen_loc, tile):
-        successor = GameState(rows=self._num_of_rows, columns=self._num_of_columns, board_layout=self._board_layout,
-                              board=copy.deepcopy(self._board), score=self._score, done=self._done,
-                              remaining_tiles=copy.deepcopy(self._tiles))
-        successor.apply_action(chosen_loc, tile)
+    def generate_successor(self, idx, tile):
+        successor = GameState(board=copy.deepcopy(self._board), score=self._score, done=self._done,
+                              remaining_tiles=self._tiles.copy())  # Use copy() for SortedSet
+        successor.apply_action(idx, tile)
         return successor
 
     def evaluate_game_state(self):
         total_score = 0
 
-        column_indices = [
-            [(2, 0), (4, 0), (6, 0)],  # Column 1
-            [(1, 1), (3, 1), (5, 1), (7, 1)],  # Column 2
-            [(0, 2), (2, 2), (4, 2), (6, 2), (8, 2)],  # Column 3
-            [(1, 3), (3, 3), (5, 3), (7, 3)],  # Column 4
-            [(2, 4), (4, 4), (6, 4)]  # Column 5
-        ]
-
-        left_diagonal_indices = [
-            [(0, 2), (1, 3), (2, 4)],  # Diagonal 1
-            [(1, 1), (2, 2), (3, 3), (4, 4)],  # Diagonal 2
-            [(2, 0), (3, 1), (4, 2), (5, 3), (6, 4)],  # Diagonal 3
-            [(4, 0), (5, 1), (6, 2), (7, 3)],  # Diagonal 4
-            [(6, 0), (7, 1), (8, 2)]  # Diagonal 5
-        ]
-
-        right_diagonal_indices = [
-            [(0, 2), (1, 1), (2, 0)],  # Diagonal 1
-            [(1, 3), (2, 2), (3, 1), (4, 0)],  # Diagonal 2
-            [(2, 4), (3, 3), (4, 2), (5, 1), (6, 0)],  # Diagonal 3
-            [(4, 4), (5, 3), (6, 2), (7, 1)],  # Diagonal 4
-            [(6, 4), (7, 3), (8, 2)]  # Diagonal 5
-        ]
-
         def calculate_score(indices, component_index):
-            first_tile = self._board[indices[0][0]][indices[0][1]]
-            if first_tile is None or first_tile[component_index] is None:
+            first_tile = self._board[indices[0]]
+            if first_tile is None:
                 return 0
 
             component_value = first_tile[component_index]
-            for (row, col) in indices[1:]:
-                tile = self._board[row][col]
+            for idx in indices[1:]:
+                tile = self._board[idx]
                 if tile is None or tile[component_index] != component_value:
                     return 0
 
             return component_value * len(indices)
 
-        for col_set in column_indices:
-            total_score += calculate_score(col_set, 0)  # Vertical component
-
-        for diag_set in left_diagonal_indices:
-            total_score += calculate_score(diag_set, 1)  # Left component
-
-        for diag_set in right_diagonal_indices:
-            total_score += calculate_score(diag_set, 2)  # Right component
+        # Calculate the score for each sequence
+        for seq, indices in seq_to_idx.items():
+            if "l" in seq:
+                total_score += calculate_score(indices, 1)  # Left component
+            elif "d" in seq:
+                total_score += calculate_score(indices, 0)  # Vertical component
+            elif "r" in seq:
+                total_score += calculate_score(indices, 2)  # Right component
 
         self._score = total_score
         return total_score

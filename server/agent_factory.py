f9dcf3c41fb494841bb1c9531fd3c7b00c8395f9
amitbe711 <amit.benbenishti@mail.huji.ac.il>
2024-08-11 17:44:17 +0300

    game to steps instead of loops, and added factory

---
+++ server/agent_factory.py
@@ -0,0 +1,18 @@
+# AgentFactory class
+from server.random_agent import RandomAgent
+
+
+class AgentFactory:
+    @staticmethod
+    def create_agent(agent_type_str: str):
+        agent_type_str = agent_type_str.lower()  # Ensure case-insensitivity
+        if agent_type_str == "Random":
+            return RandomAgent()
+        elif agent_type_str == "A-star":
+            return AStarAgent()
+        elif agent_type_str == "RL":
+            return ReinforcementLearningAgent()
+        elif agent_type_str == "Monte Carlo":
+            return MonteCarloAgent()
+        else:
+            raise ValueError(f"Unknown agent type: {agent_type_str}")

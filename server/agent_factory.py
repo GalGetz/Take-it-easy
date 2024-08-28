a66b3ba0c8c7daf8d61fd3a0d4e44e1ff3c981cd
amitbe711 <amit.benbenishti@mail.huji.ac.il>
2024-08-28 14:51:29 +0300

    Change the game_state score logic

--- server/agent_factory.py
+++ server/agent_factory.py
@@ -9,7 +9,7 @@ class AgentFactory:
         if agent_type_str == "Random":
             return RandomAgent()
         elif agent_type_str == "Expectimax":
-            return Expectimax(max_depth=4, max_actions=27, weights=[10.0, 10.0, 2.736195488285202, 0.01, 0.01])
+            return Expectimax(max_depth=4, max_actions=27, weights=[10.0000, 6.3864, 0.0100, 0.0100, 0.0100])
         elif agent_type_str == "Monte Carlo":
             return MCTSAgent(initial_simulations=400, final_simulations=600, exploration_weight=40.0)
         else:
